45a11d8441f8a116853ade4bc6b44aff8bf54c40
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-29 22:57:29 +0000

    Revert config to previous revision Usermanager basis

--- trunk/pages/usermanager.php
+++ trunk/pages/usermanager.php
@@ -8,19 +8,17 @@ if(!hasPerm(8)){
 		<h2>User Manager</h2>
 		<p>Users:</p>
 		<table cellpadding="5">
-		<tr>
-		<th>Name</td>
-		<th>BZID</td>
-		<th>Last Login</td>
-		</tr>
+		<tr><th>Name</th><th>BZID</th><th>Last Login</th><th>Manage</th></tr>
 		<?php
 		$q = mysql_query("SELECT * FROM players");
 		while($row = mysql_fetch_assoc($q)){
-			echo "<tr>\n";
-			echo '<td>'.$row['name']."</td>\n";
-			echo '<td>'.$row['bzid']."</td>\n";
-			echo '<td>'.date("m-d-Y g:i:s",$row['lastlogin'])."</td>\n";
-			echo "</tr>\n";
+			echo '<tr>';
+			echo '<td>'.$row['name'].'</td>';
+			echo '<td>'.$row['bzid'].'</td>';
+			echo '<td>'.date("m-d-Y g:i:s",$row['lastlogin']).'</td>';
+			echo '<td><a href="?p=usermanager&i='.$row['id'].'">Manage</a></td>';
+			echo '</tr>';
+			echo "\n";
 		}
 		?>
 		</table>
