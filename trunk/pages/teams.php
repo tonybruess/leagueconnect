c6a091de27402289876dd52245f49eb326b56c4d
cobrafast2@gmail.com <cobrafast2@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-29 23:10:56 +0000

    basic stuff for the team page and added sql layout.

--- trunk/pages/teams.php
+++ trunk/pages/teams.php
@@ -1,2 +1,36 @@
-		<h2>Teams</h2>
-		<p>Under Development</p>
+<h2>Teams</h2>
+<?php
+/* VARIABLES BEING READ FROM TEH QUERY STRING ON OCCASION
+ ******************************************************************
+ * $i	representing an ID (for any purposes)
+ * $a	representing an action that's being performed or prepared by the user
+ */
+
+@ $a = $_GET['a'];
+@ $i = $_GET['i'];
+
+// this is going to happen if the player hit an apply-button
+if ($a == "apply")
+{
+	echo "there's code missing here.";
+} 
+// now that we go all special cases let's do the general 
+else
+{
+?>
+<table border="0" cellspacing="2" cellpadding="1">
+<th>Name</th>
+<th>Score</th>
+<th>Leader</th>
+<?php
+	$q = mysql_query("SELECT * FROM teams ORDER BY Rank DESC"); $i = 0;
+	while ($r = mysql_fetch_array($q, MYSQL_ASSOC))
+	{
+		echo "<tr class='".rowClass($i)."'><td>".$r['Name']."</td><td>".$r['Rank']."</td><td>".getPlayerName($r['LeaderID'])."</td></tr>";
+		$i++;
+	}
+?>
+</table>
+<?php
+}
+?>
--- trunk/tables.php
+++ trunk/tables.php
@@ -4,5 +4,15 @@ create table groups (`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,`name` VARCHAR
 create table players (`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, `name` VARCHAR(100), `bzid` INT, `recordmatch` INT, `newmail` INT, `newnews` INT, `newmatch` INT, `banned` INT, `deleted` INT, `comment` TEXT, `role` INT, `firstlogin` INT, `lastlogin` INT, `country` INT, `state` INT, `email` VARCHAR(100), `aim` VARCHAR(100), `msn` VARCHAR(100), `jabber` VARCHAR(100), altnik1 VARCHAR(100), altnik2 VARCHAR(100), ircnik VARCHAR(100), emailpub INT);
 create table roles (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` TEXT,`permissions` VARCHAR(100));
 create table settings (`setting` VARCHAR(100),`value` VARCHAR(100));
+
+TABLE teams
++-----+----------+--------------+------------+------+------------+--------------+------------+----------+
+| id  | LeaderID | ColeadersIDs | PlayersIDs | Rank | Name       | CreationDate | LogoURL    | SiteText |
+|int11|  int11   |   text256    |   text256  | int8 | varchar128 |    int32     | varchar512 | text1024 |
++-----+----------+--------------+------------+------+------------+--------------+------------+----------+  
+
+* basic setup *
+insert into roles (`name`,`permissions`) VALUES ('Site Admin','11111111111111111111111');
+insert into groups (`name`,`role`,`enabled`) VALUES ('GU.LEAGUE','1','1');
 */
 ?>
