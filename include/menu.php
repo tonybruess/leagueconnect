d04b30052522f92474ea1cb547842bda787b5819
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-29 17:16:56 +0000

    Logout page Error page link fix Index page Menu redo authenticate fix style makes menu text smaller

--- include/menu.php
+++ include/menu.php
@@ -1,32 +1,31 @@
 	<div id="menu">
 		<ul>
-			<li><a href="index.php" class="active">Home</a></li>
 			<?php if($_SESSION['callsign']){ ?>
-			<li><a href="#">Mail</a></li>
-			<li><a href="#">News</a></li>
-			<li><a href="#">Matches</a></li>
-			<li><a href="#">Teams</a></li>
-			<li><a href="#">Players</a></li>
-			<li><a href="#">Help</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">Bans</a></li>
+			<li><a href="index.php" class="active">Home</a></li>
+			<li><a href="?p=mail">Mail</a></li>
 			<?php } else { ?>
 			<li><a href="http://my.bzflag.org/weblogin.php?url=http://<?php echo $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>authenticate.php%3Ftoken%3D%25TOKEN%25%26username%3D%25USERNAME%25">Login</a></li>
-			<?php } ?>		
+			<?php } ?>
+			<li><a href="?p=news">News</a></li>
+			<li><a href="?p=matches">Matches</a></li>
+			<li><a href="?p=teams">Teams</a></li>
+			<li><a href="?p=players">Players</a></li>
+			<li><a href="?p=help">Help</a></li>
+			<li><a href="?p=contact">Contact</a></li>
+			<li><a href="?p=bans">Bans</a></li>
+			<?php if($_SESSION['callsign']){ ?>
+			<li><a href="?p=logout">Logout</a></li>
+			<?php } ?>	
 		</ul>
 	</div>
 	<?php if($_SESSION['perm'][1]){ ?>
 	<div id="admin">
 		<ul>
-			<li><a href="index.php" class="active">Home</a></li>
-			<li><a href="#">Mail</a></li>
-			<li><a href="#">News</a></li>
-			<li><a href="#">Matches</a></li>
-			<li><a href="#">Teams</a></li>
-			<li><a href="#">Players</a></li>
-			<li><a href="#">Help</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">Bans</a></li>
+			<li><a href="#">Enter Match</a></li>
+			<li><a href="#">Edit Pages</a></li>
+			<li><a href="#">User Manager</a></li>
+			<li><a href="#">Team Manager</a></li>
+			<li><a href="#">Logs</a></li>
 			<?php } ?>	
 		</ul>
 	</div>
---
+++ pages/index.php
@@ -0,0 +1,2 @@
+<h2>Welcome back</h2>
+You are logged in as <?php echo $name ?>
