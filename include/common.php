4de32840e3b25bfe792a36dd5a6817edb1334438
cobrafast2@gmail.com <cobrafast2@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-29 23:03:28 +0000

    getPlayerName

--- include/common.php
+++ include/common.php
@@ -8,4 +8,12 @@ function rowClass($i){
 	if (($i%2) != 0) return "rowOdd";
 	else return "rowEven";
 }
+
+function getPlayerName($id)
+{
+	if (is_numeric($id)) {
+		$q = mysql_query("SELECT name FROM players WHERE id = ".$id);
+		while ($r = mysql_fetch_array($q, MYSQL_ASSOC)) return $r['name'];
+	}
+}
 ?>
