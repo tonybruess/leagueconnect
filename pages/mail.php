896339c0d08a2f1910893090455aff6a3d135a65
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-30 21:39:46 +0000

    Mail updates Table updates

--- pages/mail.php
+++ pages/mail.php
@@ -4,101 +4,74 @@ if(!hasPerm(2)){
 	require_once("include/footer.php");
 	die();
 }		
-?>		<h2>Mail</h2>
-<?php
-	$userid = $_SESSION['userid'];
-	$time = time();
-    // Fetch a specific message
-
-    // Flag a message as viewed
+	$ts = time();
     function read($message) {
         $sql = "UPDATE messages SET `read` = '1' WHERE `id` = '".$message."' LIMIT 1";
         return (@mysql_query($sql)) ? true:false;
     }
     
-    // Flag a message as deleted
     function deleted($message) {
         $sql = "UPDATE messages SET `to_deleted` = '1' WHERE `id` = '".$message."' LIMIT 1";
         return (@mysql_query($sql)) ? true:false;
     }
         
 	function sendmessage($to,$title,$message) {
-        $sql = "INSERT INTO messages SET `to` = '".$to."', `from` = '".$_SESSION['userid']."', `title` = '".$title."', `message` = '".$message."', `created` = '$time'";
+        $sql = "INSERT INTO messages SET `to` = '".$to."', `from` = '".$_SESSION['userid']."', `title` = '".$title."', `message` = '".$message."', `created` = '$ts'";
         return (@mysql_query($sql)) ? true:false;
     }
     
-    // BBCode
     function render($text) {
-  		$bbcode = array("<", ">",
-                "[list]", "[*]", "[/list]", 
-                "[img]", "[/img]", 
-                "[b]", "[/b]", 
-                "[u]", "[/u]", 
-                "[i]", "[/i]",
-                '[color="', "[/color]",
-                "[size=\"", "[/size]",
-                '[url="', "[/url]",
-                "[mail=\"", "[/mail]",
-                "[code]", "[/code]",
-                "[quote]", "[/quote]",
-                '"]');
-  		$htmlcode = array("&lt;", "&gt;",
-                "<ul>", "<li>", "</ul>", 
-                "<img src=\"", "\">", 
-                "<b>", "</b>", 
-                "<u>", "</u>", 
-                "<i>", "</i>",
-                "<span style=\"color:", "</span>",
-                "<span style=\"font-size:", "</span>",
-                '<a href="', "</a>",
-                "<a href=\"mailto:", "</a>",
-                "<code>", "</code>",
-                "<img src=\"img/quote1.jpg\">", "<img src=\"img/quote2.jpg\">",
-                '">');
+  		$bbcode = array("<", ">", "[list]", "[*]", "[/list]", "[img]", "[/img]", "[b]", "[/b]", "[u]", "[/u]", "[i]", "[/i]", '[color="', "[/color]", "[size=\"", "[/size]", '[url="', "[/url]", "[mail=\"", "[/mail]", "[code]", "[/code]", "[quote]", "[/quote]", '"]');
+  		$htmlcode = array("&lt;", "&gt;", "<ul>", "<li>", "</ul>", "<img src=\"", "\">", "<b>", "</b>", "<u>", "</u>", "<i>", "</i>", "<span style=\"color:", "</span>", "<span style=\"font-size:", "</span>", '<a href="', "</a>", "<a href=\"mailto:", "</a>", "<code>", "</code>", "<img src=\"img/quote1.jpg\">", "<img src=\"img/quote2.jpg\">", '">');
  		$newtext = str_replace($bbcode, $htmlcode, $text);
-  		$newtext = nl2br($newtext);//second pass
+  		$newtext = nl2br($newtext); //second pass
   		return $newtext;
 	}
+	
     // check if a new message had been sent
     if(isset($_POST['newmessage'])) {
         // error while sending message?
         if($_POST['to'] !== "" && $_POST['subject'] !== "" && $_POST['message'] !== ""){
-        	if(sendmessage($_POST['to'],$_POST['subject'],$_POST['message'])) {
+        	$toclean = MySQL::sanitize($_POST['to']);
+        	$subclean = MySQL::sanitize($_POST['subject']);
+        	$mesclean = MySQL::sanitize($_POST['message']);
+        	if(sendmessage($toclean,$subclean,$mesclean)) {
             // Goood
-            echo "Message successfully sent to ".getPlayerName($_POST['to']);
-            echo "<br><br>";
-        } else {
-            // No Good
-            echo "Error, couldn't send PM. Maybe wrong user.";
-            echo "<br><br>";
-            $fail = 1;
-        }
-    } else {
-    	echo "Missing Data";
-		echo "<br><br>";
-		$fail = 1;
+            	echo "Message successfully sent to ".getPlayerName($_POST['to']);
+            	echo "<br><br>";
+        	} else {
+            	// No Good
+            	echo "Error, couldn't send PM. Maybe wrong user.";
+            	echo "<br><br>";
+            	$fail = 1;
+        	}
+    	} else {
+    		echo "Missing Data";
+			echo "<br><br>";
+			$fail = 1;
 		}
     }
-    // Delted message?
+    
+    // Were deleting a message
     if(isset($_POST['delete'])) {
-        // Error deleting?
         if(deleted($_POST['did'])) {
             echo 'Message deleted successfully!';
             echo "<br><br>";
         } else {
-            echo "Error, couldn't delete PM!";
+            echo "Error, couldn't delete PM";
             echo "<br><br>";
         }
     }
     
 ?>
+<h2>[<a href="?p=mail&op=compose">Compose</a>] - [<a href="?p=mail&op=new">Inbox</a>] - [<a href="?p=mail&op=sent">Outbox</a>]</h2>
 <script type="text/javascript" src="global/bbeditor/ed.js"></script>  
-<a href="?p=mail&op=compose">Compose</a> - 
-<a href="?p=mail&op=new">Read</a>
 <?php
 // If unspecified/recgonized page, show all messages
 if(!isset($_GET['op']) || $_GET['op'] == 'new') {
 ?>
+<a href="?p=mail&op=compose">Compose</a> - 
+<a href="?p=mail&op=new">Read</a>
 <table border="0" cellspacing="2" cellpadding="3">
     <tr>
         <td>From</td>
@@ -107,17 +80,20 @@ if(!isset($_GET['op']) || $_GET['op'] == 'new') {
     </tr>
     <?php
         // If there are messages, show them
-   		$sql = "SELECT * FROM messages WHERE `to` = '".$userid."' && `to_deleted`='0'";
+   		$sql = "SELECT * FROM messages WHERE `to` = '".CurrentPlayer::$ID."' && `to_deleted`='0'";
 		$result = mysql_query($sql) or die (mysql_error());
         // Check if there are any results
         if(mysql_num_rows($result)) {
             // if yes, fetch them!
             while($message = mysql_fetch_assoc($result)) {
+            	$unread = false;
+            	if($message['read']=='0')
+            		$unread = true;
                 ?>
                 <tr>
                     <td><?php echo getPlayerName($message['from']); ?></td>
-                    <td><a href='<?php echo $_SERVER['PHP_SELF']; ?>?p=mail&op=view&mid=<?php echo $message['id']; ?>'><?php echo $message['title'] ?></a></td>
-                    <td><?php echo $message['created']; ?></td>
+                    <td><a href='?p=mail&op=view&mid=<?php echo $message['id']; ?>'<?php if($unread) echo 'style="color: red;"'; ?>><?php echo $message['title'] ?></a></td>
+                    <td><?php echo date("m-d-Y",$message['ts']); ?></td>
                 </tr>
                 <?php
             }
@@ -131,7 +107,7 @@ if(!isset($_GET['op']) || $_GET['op'] == 'new') {
 } elseif($_GET['op'] == 'view' && isset($_GET['mid'])) {
 	$messageid = $_GET['mid'];
 	// Set message to viewed
-	$sql = "SELECT * FROM messages WHERE `id` = '$messageid' && (`from` = '".$userid."' || `to` = '".$userid."') LIMIT 1";
+	$sql = "SELECT * FROM messages WHERE `id` = '$messageid' && (`from` = '".CurrentPlayer::$ID."' || `to` = '".CurrentPlayer::$ID."') LIMIT 1";
 	$result = mysql_query($sql);
 	if(mysql_num_rows($result)) {
 	// fetch the data
@@ -184,7 +160,7 @@ $query = "SELECT * FROM players ORDER BY name asc";
 $result = mysql_query($query) or die(mysql_error());
 while($row = mysql_fetch_array($result)){
 	echo '<option value="'.$row['id'].'"';
-	if($row['id'] == $_POST['rfrom'] || $row['id'] == $_POST['to']) echo " selected";
+	if($row['id'] == $_POST['from'] || $row['id'] == $_POST['to']) echo " selected";
 	echo '>'.$row['name'].'</option>';
 }
 ?>
