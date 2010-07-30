aa9a230c6f9c7071186479fc9264a35a380c1c87
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-30 20:38:52 +0000

    Document some of the fields of the MySQL database tables

--- trunk/tables.sql
+++ trunk/tables.sql
@@ -1,74 +1,74 @@
 
 create table groups (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(100),
-    `role` BIGINT,
-    `enabled` INT
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for this group
+    `name` VARCHAR(100), -- Name of the group
+    `role` BIGINT, -- ???
+    `enabled` BOOLEAN NOT NULL DEFAULT FALSE -- TRUE if enabled, FALSE otherwise
 );
 
 create table players (
-    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(100),
-    `bzid` INT UNSIGNED,
-    `team` INT UNSIGNED,
-    `recordmatch` INT,
-    `newmail` INT UNSIGNED,
-    `newnews` INT UNSIGNED,
-    `newmatch` INT UNSIGNED,
-    `comment` TEXT,
-    `firstlogin` TIMESTAMP,
-    `lastlogin` TIMESTAMP,
-    `country` VARCHAR(100),
-    `state` VARCHAR(100),
-    `email` VARCHAR(100),
-    `aim` VARCHAR(100),
-    `msn` VARCHAR(100),
-    `jabber` VARCHAR(100),
-    `altNicks` TEXT,
-    `ircnick` VARCHAR(100),
-    `pubemail` VARCHAR(100),
-    `banned` BOOLEAN,
-    `deleted` BOOLEAN
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for each player
+    `name` VARCHAR(100), -- Callsign
+    `bzid` INT UNSIGNED, -- BZFlag callsign unique identifier
+    `team` INT UNSIGNED, -- ID of the team this player is a member of
+    `recordmatch` INT, -- ???
+    `newmail` BOOLEAN, -- TRUE if mail awaits, FALSE otherwise
+    `newnews` BOOLEAN, -- TRUE if news awaits, FALSE otherwise
+    `newmatch` BOOLEAN, -- TRUE if a match request awaits, FALSE otherwise
+    `comment` TEXT, -- Comment?
+    `firstlogin` TIMESTAMP, -- Time when the player first logged in
+    `lastlogin` TIMESTAMP,  -- Time when the player last logged in
+    `country` VARCHAR(100), -- Country of residence
+    `state` VARCHAR(100),   -- State of residence
+    `email` VARCHAR(100), -- Email address
+    `aim` VARCHAR(100), -- AOL Instant Messenger address
+    `msn` VARCHAR(100), -- MSN Instant Messenger address
+    `jabber` VARCHAR(100), -- Jabber address
+    `altNicks` TEXT, -- Alternative nicknames
+    `ircnick` VARCHAR(100), -- IRC nickname
+    `pubemail` VARCHAR(100), -- Public Email?
+    `banned` BOOLEAN, -- TRUE if banned, FALSE if not
+    `deleted` BOOLEAN -- TRUE if deleted, FALSE if not
 );
 
 create table roles (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` TEXT,
-    `permissions` VARCHAR(100)
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for this role
+    `name` TEXT, -- Name of the role
+    `permissions` VARCHAR(100) -- Permissions granted to this role
 );
 
 create table settings (
-    `setting` VARCHAR(100),
+    `setting` VARCHAR(100) PRIMARY KEY,
     `value` VARCHAR(100)
 );
 
 create table teams (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(128),
-    `created` TIMESTAMP,
-    `leader` UNSIGNED INT,
-    `coleaders` TEXT,
-    `activity` INT,
-    `rank` UNSIGNED INT,
-    `logourl` VARCHAR(128),
-    `description` TEXT,
-    `closed` BOOLEAN,
-    `inactive` BOOLEAN,
-    `deleted` BOOLEAN
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, -- Unique identifier for this team
+    `name` VARCHAR(128), -- Name of the team
+    `created` TIMESTAMP, -- Date the team was created
+    `leader` UNSIGNED INT, -- Player ID of the leader of the team
+    `coleaders` TEXT, -- List of player IDs for the co-leaders
+    `activity` INT, -- Activity?
+    `rank` UNSIGNED INT, -- Rank of this team
+    `logo` VARCHAR(128), -- URL for the team's logo
+    `description` TEXT, -- Description of the team
+    `closed` BOOLEAN, -- TRUE if this is closed and no one may join, FALSE otherwise
+    `inactive` BOOLEAN, -- TRUE if this team is inactive, FALSE otherwise
+    `deleted` BOOLEAN -- TRUE if this team is deleted, FALSE otherwise
 );
 
 create table messages (
-    `id` NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    `title` VARCHAR( 255 ) NULL ,
-    `message` TEXT NOT NULL ,
-    `from` INT( 11 ) NOT NULL ,
-    `to` INT( 11 ) NOT NULL ,
-    `read` BOOL NOT NULL DEFAULT '0',
-    `from_deleted` BOOL NOT NULL DEFAULT '0',
-    `to_deleted` BOOL NOT NULL DEFAULT '0',
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for this message
+    `subject` VARCHAR( 255 ) NULL , -- Subject of the message
+    `message` TEXT NOT NULL , -- Contents of the message
+    `from` INT( 11 ) NOT NULL , -- Player ID who sent the message
+    `to` INT( 11 ) NOT NULL , -- Player ID of the recepient
+    `read` BOOLEAN NOT NULL DEFAULT FALSE, -- TRUE if read, FALSE otherwise
+    `from_deleted` BOOLEAN NOT NULL DEFAULT FALSE, -- ???
+    `to_deleted` BOOLEAN NOT NULL DEFAULT FALSE, -- ???
     `created` TIMESTAMP NOT NULL
  );
 
 -- basic setup
 insert into roles (`name`,`permissions`) VALUES ('Site Admin','11111111111111111111111');
-insert into groups (`name`,`role`,`enabled`) VALUES ('GU.LEAGUE','1','1');
+insert into groups (`name`,`role`,`enabled`) VALUES ('GU.LEAGUE', '1', TRUE);
