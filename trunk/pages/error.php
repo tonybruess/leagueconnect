7c95b33b2c43de5a5578bcd38c262f73603f6dd3
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-07-31 01:03:15 +0000

    $sitedata['email'] doesn't exist, replace it with something that does

--- trunk/pages/error.php
+++ trunk/pages/error.php
@@ -10,17 +10,17 @@
 				<p>You have successfully logged out. Would you like to <a href="./">login</a> again?</p>
 			<?php } elseif($_GET['error'] == "3") {  ?>
 				<h2>No Permission</h2>
-				<p>Your account has been temporarily disabled by an administrator. Please contact <?php echo $sitedata['email'] ?> for more information</p>
+				<p>Your account has been temporarily disabled by an administrator. Please contact <?php echo Config::ContactEmail; ?> for more information</p>
 			<?php } elseif($_GET['error'] == "4") {  ?>
 				<h2>No Permission</h2>
 				<p>Your username and password was correct, but you do not have any permissions on this server.</p>
-				<p>If you believe this message is in error, please contact <?php echo $sitedata['email'] ?> for more information</p>
+				<p>If you believe this message is in error, please contact <?php echo Config::ContactEmail; ?> for more information</p>
 			<?php } elseif(!$_GET['error']) {  ?>
 				<h2>Please login</h2>
 				<p>Please <a href="index.php">login</a></p>
 			<?php } elseif($_GET['mode'] == "") {  ?>
 				<h2>Unknown Error</h2>
-				<p>Please contact <?php echo $sitedata['email'] ?> with error code: <?php echo $_GET['error'].rand(100000,999999); ?> </p>
+				<p>Please contact <?php echo Config::ContactEmail; ?> with error code: <?php echo $_GET['error'].rand(100000,999999); ?> </p>
 			<?php 
 			}
 			}
