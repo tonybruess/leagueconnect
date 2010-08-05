fcc847175c664467b3c277fe344f853e6ca65519
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-05 18:52:42 +0000

    News updates Index status Encode http

--- pages/news.php
+++ pages/news.php
@@ -1,9 +1,27 @@
 		<h2>News</h2>
-		<div id="item">
-			<div id="header">
-				<div id="author">By: Author</div>
-				<div id="time">Time</div>
-			</div>
-			<div id="data">Body</div>
-		</div>
-		<br>
+		<p>[<a href="?p=news&op=new">New Entry</a>]</p>
+		<?php
+		if($_GET['op'] == 'new')
+		{
+			if($_POST['edit']){
+			}
+		?>
+		<form method="POST">
+		Author:
+		<select name="author">
+		<option value="" disabled>Select a User</option><?php
+		$query = "SELECT * FROM players ORDER BY name asc"; 
+		$result = mysql_query($query) or die(mysql_error());
+		while($row = mysql_fetch_array($result)){
+			echo '<option value="'.$row['id'].'"';
+			if($row['id'] == CurrentPlayer::$ID) echo " selected";
+			echo '>'.$row['name'].'</option>';
+		}
+		?>
+		</select><br><br>
+		</form>
+		<?php
+		} else {
+			MySQL::GetItems(1);
+		}
+		?>
