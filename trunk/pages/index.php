c02808dafa458fbc2673a9cad007ef50ea0f951b
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-05 18:52:42 +0000

    News updates Index status Encode http

--- trunk/pages/index.php
+++ trunk/pages/index.php
@@ -2,7 +2,11 @@
 
 require_once('./include/current-player.php');
 
+$result = MySQL::Query("SELECT id FROM messages WHERE `read`='0' AND `to`='CurrentPlayer::$ID'");
+$messages = mysql_num_rows($result);
 ?>
 
 <h2>Welcome back</h2>
-You are logged in as <?php echo CurrentPlayer::$Name; ?>.
+You are logged in as <?php echo CurrentPlayer::$Name; ?>
+<br><br>
+You have <?php echo $messages; ?> new message<?php if($messages !== 1) echo 's' ?>
