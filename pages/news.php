db0a85be926f13bce86435f24a83c15a7dde565c
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-05 00:52:23 +0000

    Basic layout for upcoming pages

--- pages/news.php
+++ pages/news.php
@@ -1,2 +1,9 @@
 		<h2>News</h2>
-		<p>Under Development</p>
+		<div id="item">
+			<div id="header">
+				<div id="author">By: Author</div>
+				<div id="time">Time</div>
+			</div>
+			<div id="data">Body</div>
+		</div>
+		<br>
