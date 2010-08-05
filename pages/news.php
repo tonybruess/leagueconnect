ea3c37e0566970b444422baf2e5e9eeae23bd3d2
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-05 19:33:52 +0000

    News

--- pages/news.php
+++ pages/news.php
@@ -3,24 +3,34 @@
 		<?php
 		if($_GET['op'] == 'new')
 		{
-			if($_POST['edit']){
+			if($_POST){
+				$author = MySQL::Sanitize($_POST['author']);
+				$message = MySQL::Sanitize($_POST['message']);
+				$date = MySQL::Sanitize($_POST['date']) . ' ' . MySQL::Sanitize($_POST['time']);
+				$page = '1';
+				if(mysql_query("INSERT INTO entries SET `author`='$author', `message`='$message', `created`='$date', `page`='$page'"))
+					echo "Posted new entry successfully";
+					echo mysql_error();
 			}
 		?>
 		<form method="POST">
 		Author:
-		<select name="author">
-		<option value="" disabled>Select a User</option><?php
-		$query = "SELECT * FROM players ORDER BY name asc"; 
-		$result = mysql_query($query) or die(mysql_error());
-		while($row = mysql_fetch_array($result)){
-			echo '<option value="'.$row['id'].'"';
-			if($row['id'] == CurrentPlayer::$ID) echo " selected";
-			echo '>'.$row['name'].'</option>';
-		}
-		?>
-		</select><br><br>
+		<input type="text" name="author" value="<?php echo CurrentPlayer::$Name ?>">
+		<br><br>
+		Message:
+		<br>
+		<textarea cols=50 rows=10 name="message"></textarea>
+		<br><br>
+		Date: <input type="text" name="date" value="<?php echo date("Y-m-d") ?>" maxlength="10" style="width: 70px;">
+		<br><br>
+		Time: <input type="text" name="time" value="<?php echo date("H:i:s") ?>" maxlength="8" style="width: 50px;">
+		<br><br>
+		Page: <?php echo MySQL::getPageName("News"); ?>
+		<br><br>
+		<input type="submit" value="Save">
 		</form>
 		<?php
+		} elseif($_POST['edit']){
 		} else {
 			MySQL::GetItems(1);
 		}
