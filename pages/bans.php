3c5b93fe7cf3a3bd68819de62c51da16915bf8a4
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-06 22:03:53 +0000

    Edit entries

--- pages/bans.php
+++ pages/bans.php
@@ -8,9 +8,7 @@
                 $date = $_POST['date'] . ' ' . $_POST['time'];
 
                 if(MySQL::AddEntry($_POST['author'],$_POST['message'],$date,4))
-                {
                     echo "Posted new entry successfully.";
-                }
             }
             ?>
 
@@ -35,9 +33,40 @@
 
             <?php
         }
-        elseif($_POST['edit'])
+        elseif($_GET['op'] == 'edit' && $_GET['i'])
         {
-        // TODO: Edit already posted news items
+            $id = MySQL::Sanitize($_GET['i']);
+            if($_POST)
+            {
+                $date = $_POST['date'] . ' ' . $_POST['time'];
+                
+                if(MySQL::UpdateEntry($_POST['author'],$_POST['message'],$date,4,$_POST['id']))
+                    echo 'Updated entry successfully';
+            }
+            
+            $entry = mysql_fetch_assoc(MySQL::Query("SELECT * FROM bans WHERE id='$id' LIMIT 1"));
+            $date = explode(' ',$entry['created']);
+        ?>
+            <form method="POST">
+            Author:
+            <input type="text" name="author" value="<?php echo $entry['author'] ?>">
+            <br><br>
+            Message:
+            <br>
+            <script type="text/javascript" src="global/bbeditor/ed.js"></script>
+            <script>AddBBCodeToolbar('message'); </script>
+            <textarea cols=50 rows=10 name="message" id="message"><?php echo $entry['message'] ?></textarea>
+            <br><br>
+            Date: <input type="text" name="date" value="<?php echo $date[0]; ?>" maxlength="10" style="width: 70px;">
+            <br><br>
+            Time: <input type="text" name="time" value="<?php echo $date[1]; ?>" maxlength="8" style="width: 50px;">
+            <br><br>
+            Page: <?php echo MySQL::GetPageName("Bans"); ?>
+            <br><br>
+            <input type="hidden" name="id" value="<?php echo $entry['id'] ?>">
+            <input type="submit" value="Save">
+            </form>
+        <?php
         }
         else
         {
