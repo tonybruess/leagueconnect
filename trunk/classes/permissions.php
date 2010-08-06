5bdb564f548a2eb6f8ce176da1341bcf1379a10c
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-06 21:29:17 +0000

    Menu and Permissions

--- trunk/classes/permissions.php
+++ trunk/classes/permissions.php
@@ -5,11 +5,26 @@
 
 class Permissions
 {
-    const SendMail = 2;
-    const EnterMatch = 4;
-    const ManageTeams = 5;
+    const Login = 1;
+    const ViewMail = 2;
+    const SendMail = 3;
+    const EditMail = 4;
+    const ViewPages = 5;
+    const AddPages = 6;
     const EditPages = 7;
-    const ViewLogs = 9;
+    const ViewMatches = 8;
+    const AddMatches = 9;
+    const EditMatches = 10;
+    const ViewTeams = 11;
+    const AddTems = 12;
+    const JoinTeams = 13;
+    const EditTeams = 14;
+    const ReviveTeams = 15;
+    const ViewPlayers = 16;
+    const EditPlayers = 17;
+    const DeletePlayers = 18;
+    const EnterMatch = 19;
+    const ViewLogs = 20;
 }
 
 ?>
