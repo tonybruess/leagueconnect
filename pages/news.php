4319c9ed88381968bb9ff75e64cc296166a526db
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-06 04:41:11 +0000

    HTML should stay away from database code.

--- pages/news.php
+++ pages/news.php
@@ -5,11 +5,35 @@
 		{
 			if($_POST)
 			{
-				$date = $_POST['date'] . ' ' . $_POST['time'];				
-				if(MySQL::addItem($_POST['author'],$_POST['message'],$date,1))
-					echo "Posted new entry successfully";
+				$date = $_POST['date'] . ' ' . $_POST['time'];
+
+				if(MySQL::AddItem($_POST['author'],$_POST['message'],$date,1))
+                {
+					echo "Posted new entry successfully.";
+                }
 			}
-			MySQL::newItemForm();
+            ?>
+
+            <form method="POST">
+            Author:
+            <input type="text" name="author" value="<?php echo CurrentPlayer::$Name ?>">
+            <br><br>
+            Message:
+            <br>
+            <script type="text/javascript" src="global/bbeditor/ed.js"></script>
+            <script>edToolbar('message'); </script>
+            <textarea cols=50 rows=10 name="message" id="message"></textarea>
+            <br><br>
+            Date: <input type="text" name="date" value="<?php echo date("Y-m-d") ?>" maxlength="10" style="width: 70px;">
+            <br><br>
+            Time: <input type="text" name="time" value="<?php echo date("H:i:s") ?>" maxlength="8" style="width: 50px;">
+            <br><br>
+            Page: <?php echo MySQL::GetPageName("News"); ?>
+            <br><br>
+            <input type="submit" value="Save">
+            </form>
+
+            <?php
 		}
 		elseif($_POST['edit'])
 		{
