958a889c40ef8ea2870d94b395bdc276033a2cfe
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-08 19:58:46 +0000

    Start PDO integration.

---
+++ trunk/include/logging.php
@@ -0,0 +1,24 @@
+<?php
+/* 
+ * Log errors and other details.
+ */
+
+require_once('config.php');
+
+class Logging
+{
+    /* void */ public static function LogError()
+    {
+        $error = implode("\n", func_get_args());
+
+        $msg = date('l F jS, Y at g:i:sA') . "\n"
+             . $error . "\n"
+             . "----------\n";
+
+        file_put_contents(Config::ErrorLogFile, $msg, FILE_APPEND);
+    }
+}
+
+file_put_contents(Config::ErrorLogFile, '');
+
+?>
