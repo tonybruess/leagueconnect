c887da82f60a98d435b3a2f4b8e1efc2334dd176
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-10 18:22:03 +0000

    Add folders for images, javascript, and css.

---
+++ trunk/system/application/views/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>League Connect Test</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="top">
+		<div id="logo"><a href="">League Connect Test</a></div>
+		<div id="statusbar">
+			<ul>
+				<li><a href='?p=index'>Mail</a></li>
+				<li><a href='?p=index'>Logout</a></li>
+				<li>Logged in as: <a href="#">Test</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="menu">
+		<ul>
+			<li><a href='?p=index' class="active">Home</a></li>
+			<li><a href='?p=news'>News</a></li>
+			<li><a href='?p=matches'>Matches</a></li>
+			<li><a href='?p=teams'>Teams</a></li>
+			<li><a href='?p=players'>Players</a></li>
+			<li><a href='?p=help'>Help</a></li>
+			<li><a href='?p=contact'>Contact</a></li>
+			<li><a href='?p=bans'>Bans</a></li>
+			<li><a href='?p=cp'>Control Panel</a></li>
+		</ul>
+	</div>
+	<div id="page_title">
+		<h3>Welcome</h3>
+	</div>
+	<div id="page_navigation">
+		<a href="?p=index">Edit Page</a>
+	</div>
+	<div id="page_body">
+		Welcome to the beta version of league connect.
+		<br><br>
+		This is currently a work in progress. Please report all bugs <a href="http://code.google.com/p/leagueconnect/issues/entry">here</a>
+	</div>
+	<div id="footer">League Connect v. 0.01 B1 - <a href="http://code.google.com/p/leagueconnect/source/browse/#svn/trunk">Source Code</a></div>
+</body>
+</html>
