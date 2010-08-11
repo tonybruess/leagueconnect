c93b2e82bf48fdbe5bc2b65af13119fa41712bf3
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-11 18:05:49 +0000

    Fix urls.

--- trunk/system/application/views/menu.php
+++ trunk/system/application/views/menu.php
@@ -1,19 +1,19 @@
     <div id="menu">
         <ul>
-            <li><a href='?p=index' class="active">Home</a></li>
-            <li><a href='?p=news'>News</a></li>
-            <li><a href='?p=matches'>Matches</a></li>
-            <li><a href='?p=teams'>Teams</a></li>
-            <li><a href='?p=players'>Players</a></li>
-            <li><a href='?p=help'>Help</a></li>
-            <li><a href='?p=contact'>Contact</a></li>
-            <li><a href='?p=bans'>Bans</a></li>
-            <li><a href='?p=cp'>Control Panel</a></li>
+            <li><a href='{site_url path="index"}' class="active">Home</a></li>
+            <li><a href='{site_url path="news"}'>News</a></li>
+            <li><a href='{site_url path="matches"}'>Matches</a></li>
+            <li><a href='{site_url path="teams"}'>Teams</a></li>
+            <li><a href='{site_url path="players"}'>Players</a></li>
+            <li><a href='{site_url path="help"}'>Help</a></li>
+            <li><a href='{site_url path="contact"}'>Contact</a></li>
+            <li><a href='{site_url path="bans"}'>Bans</a></li>
+            <li><a href='{site_url path="control-panel"}'>Control Panel</a></li>
         </ul>
     </div>
     <div id="page_navigation">
         {section name=i loop=$MenuItems}
         {$Name = $MenuItems[i].Name}
-        <a href="/page/show/{$Name}">{$Name}</a>
+        <a href='{site_url path="page/show/$Name"}'>{$Name}</a>
         {/section}
     </div>
