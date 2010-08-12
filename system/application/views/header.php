f0997a223ce908c8983675cde9fea04ac4e48ffb
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-12 18:22:49 +0000

    Fix urls.

--- system/application/views/header.php
+++ system/application/views/header.php
@@ -9,8 +9,8 @@
         <div id="logo"><a href="">{$Title}</a></div>
         <div id="statusbar">
             <ul>
-                <li><a href='?p=index'>Mail</a></li>
-                <li><a href='?p=index'>Logout</a></li>
+                <li><a href='{site_url path="mail"}'>Mail</a></li>
+                <li><a href='{site_url path="logout"}'>Logout</a></li>
                 <li>Logged in as: <a href="#">{$Username}</a></li>
             </ul>
         </div>
