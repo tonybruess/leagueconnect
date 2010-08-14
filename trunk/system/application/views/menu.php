967fbedfd674c70eb3fbf4266484a5ec8875f967
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-14 19:06:32 +0000

    Bugfix.

--- trunk/system/application/views/menu.php
+++ trunk/system/application/views/menu.php
@@ -1,6 +1,6 @@
     <div id="menu">
         <ul>
-            <li><a href='{site_url path="index"}' class="active">Home</a></li>
+            <li><a href='{site_url path="home"}' class="active">Home</a></li>
             <li><a href='{site_url path="news"}'>News</a></li>
             <li><a href='{site_url path="matches"}'>Matches</a></li>
             <li><a href='{site_url path="teams"}'>Teams</a></li>
