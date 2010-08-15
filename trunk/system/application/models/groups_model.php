8fcfc58304922d6fbdbfca8e3a0ec19cd17ff559
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-15 18:50:16 +0000

    Implement auth.

--- trunk/system/application/models/groups_model.php
+++ trunk/system/application/models/groups_model.php
@@ -16,6 +16,22 @@ class Groups_Model extends Model
     {
         return $this->db->get('groups')->where('id', $id)->row();
     }
+
+    function getTotalPermissions($groups)
+    {
+        $totalPerms = 0;
+
+        foreach($groups as $group)
+        {
+            $role = $this->db->select('role')->get('groups')->where('name', $group)->row();
+            $perms = $this->db->select('permissions')->get('roles')->where('id', $role['role']);
+            $perms = (int)$perms;
+
+            $totalPerms |= $perms;
+        }
+
+        return $totalPerms;
+    }
 }
 
 ?>
