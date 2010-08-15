1261d932ba5298f690e2bba47ff8e45f0d8f4d39
yassen.bz@gmail.com <yassen.bz@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-15 21:53:45 +0000

    Another bugfix.

--- system/application/models/groups_model.php
+++ system/application/models/groups_model.php
@@ -33,7 +33,7 @@ class Groups_Model extends Model
         foreach($groups as $group)
         {
             $role = $this->db->select('role')->where('name', $group)->get('groups')->row();
-            $perms = $this->db->select('permissions')->get('roles')->where('id', $role['role']);
+            $perms = $this->db->select('permissions')->where('id', $role['role'])->get('roles');
             $perms = (int)$perms;
 
             $totalPerms |= $perms;
