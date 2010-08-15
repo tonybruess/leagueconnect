13f3202af6e0835e9c8737ad7465a81f92b888fb
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-15 20:14:33 +0000

    Menu and home are displaying nav wrong

--- system/application/views/home.php
+++ system/application/views/home.php
@@ -1,8 +1,10 @@
     <div id="page_title">
         <h3>Welcome</h3>
     </div>
-    <div id="page_navigation">
-        <a href='{site_url path="page/edit/home"}'>Edit Page</a>
+   <div id="page_navigation">
+{foreach from=$MenuItems item=item}
+        <a href='{site_url path="page/show/{$item.Name}"}'>{$item.Name}</a>
+{/foreach}
     </div>
     <div id="page_body">
         Welcome to the beta version of league connect.
