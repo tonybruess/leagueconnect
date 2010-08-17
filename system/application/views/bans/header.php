2653dbc13a4125395ed990c3c117483a05894deb
tonybruess@gmail.com <tonybruess@gmail.com@b001015b-d9ac-b37f-88f6-f159509f8fe3>
2010-08-17 22:46:39 +0000

    Proper spacing makes the page look nice <3

--- system/application/views/bans/header.php
+++ system/application/views/bans/header.php
@@ -1,7 +1,7 @@
-        <div id="page_title">
-            <h3>Bans</h3>
-        </div>
-        <div id="page_navigation">
-            <a href='{site_url path="bans"}'>View Entries</a>
-            {if {has_perm perm='AddPages'}}<a href='{site_url path="bans/add"}'>New Entry</a>{/if}
-        </div>
+    <div id="page_title">
+        <h3>Bans</h3>
+    </div>
+    <div id="page_navigation">
+        <a href='{site_url path="bans"}'>View Entries</a>
+{if {has_perm perm='AddPages'}}        <a href='{site_url path="bans/add"}'>New Entry</a>
+{/if}    </div>
---
+++ system/application/views/news/header.php
@@ -0,0 +1,7 @@
+        <div id="page_title">
+            <h3>News</h3>
+        </div>
+        <div id="page_navigation">
+            <a href='{site_url path="news"}'>View Entries</a>
+            {has_perm perm='AddPages'}<a href='{site_url path="bans/add"}'>New Entry</a>{/has_perm}
+        </div>
